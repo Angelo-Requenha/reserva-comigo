ee3972901ec381c65d6936101fbb818ad4bc668a
unknown <angelorequenha@gmail.com>
2024-01-12 17:23:34 -0300

    ok

--- reserva_app/views.py
+++ reserva_app/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'reserva_app/login.html')
+    return render(request, 'register/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Faça o login do usuário após o registro
+            return redirect('/')  # Redirecione para a página de perfil após o registro
+    else:
+        form = CustomUserCreationForm()
+        
+    context = {
+        'form': form
+    }
+    
+    return render(request, 'registration/register.html', context)
