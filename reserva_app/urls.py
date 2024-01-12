b7f6d4bd98482bead8af81316d8a22f041b03649
Isabela <isabela.bnu@gmail.com>
2024-01-12 18:43:39 -0300

    pagina inicial

--- reserva_app/urls.py
+++ reserva_app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import login
+from .views import login, init_page
 
 app_name = 'reserva_app'
 
 urlpatterns = [
     path('', login, name='login'),
+    path('init_page', init_page, name='init_page'),
 ]
--- reserva_app/views.py
+++ reserva_app/views.py
@@ -6,18 +6,6 @@ from .forms import CustomUserCreationForm
 def login(request):
     return render(request, 'register/login.html')
 
-def register(request):
-    if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)  # Faça o login do usuário após o registro
-            return redirect('/')  # Redirecione para a página de perfil após o registro
-    else:
-        form = CustomUserCreationForm()
-        
-    context = {
-        'form': form
-    }
-    
-    return render(request, 'registration/register.html', context)
+
+def init_page(request):
+    return render(request, 'pages/init_page.html')
