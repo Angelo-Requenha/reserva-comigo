748e31dcd34030b06ba3c8c4909911e8ca40412f
Felipe <felipebs779@gmail.com>
2024-01-17 16:17:26 -0300

    erro de css

--- reserva_app/urls.py
+++ reserva_app/urls.py
@@ -8,6 +8,6 @@ app_name = 'reserva_app'
 urlpatterns = [
     path('cadastro/', cadastro, name='cadastro'),
     path('login/', login, name='login'),
-    path('init_page/', init_page, name='init_page'),
+    path('', init_page, name='init_page'),
     path('sobre_nos/class', sobre_nos, name='sobre_nos')
 ]
