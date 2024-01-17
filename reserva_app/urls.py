54b27ca7348a5073cb601aa6f0c544d869177da3
Felipe <felipebs779@gmail.com>
2024-01-17 14:25:10 -0300

    organizando os arquivos de estilo

--- reserva_app/urls.py
+++ reserva_app/urls.py
@@ -6,7 +6,7 @@ from .views import init_page, cadastro, login
 app_name = 'reserva_app'
 
 urlpatterns = [
-    path('', cadastro, name='cadastro'),
+    path('cadastro/', cadastro, name='cadastro'),
     path('login/', login, name='login'),  # Certifique-se de que o caminho tenha uma barra final
-    path('init_page/', init_page, name='init_page'),
+    path('', init_page, name='init_page'),
 ]
