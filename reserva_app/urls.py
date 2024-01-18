49a19a5b2399159349c9803f2764074d0325f745
Daniel Nunes <daniel.nunes7010@gmail.com>
2024-01-18 14:28:06 -0300

    criando a página inicial para o user

--- reserva_app/urls.py
+++ reserva_app/urls.py
@@ -1,7 +1,7 @@
 # urls.py
 from django.contrib import admin
 from django.urls import path, include
-from .views import init_page, cadastro, login, sobre_nos
+from .views import init_page, cadastro, login, sobre_nos, init_page_usuario
 
 app_name = 'reserva_app'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('cadastro/', cadastro, name='cadastro'),
     path('login/', login, name='login'),
     path('', init_page, name='init_page'),
-    path('sobre_nos/class', sobre_nos, name='sobre_nos')
+    path('sobre_nos/', sobre_nos, name='sobre_nos'),
+    path('init_page_usuario/', init_page_usuario, name='init_page_usuario'),
 ]
