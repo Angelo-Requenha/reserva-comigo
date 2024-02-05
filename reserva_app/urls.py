70965ca9fc7498dd3c5c8bcefff9a0139da9a365
victormendexx <victorgomesmendes05@gmail.com>
2024-02-05 14:18:32 -0300

    mudando as cores da página convidativa

--- reserva_app/urls.py
+++ reserva_app/urls.py
@@ -6,9 +6,9 @@ from .views import init_page, sobre_nos, init_page_usuario, home, pagina_convida
 app_name = 'reserva_app'
 
 urlpatterns = [
-    path('', init_page, name='init_page'),
+    path('init_page', init_page, name='init_page'),
     path('sobre_nos/', sobre_nos, name='sobre_nos'),
     path('init_page_usuario/', init_page_usuario, name='init_page_usuario'),
     path('home', home, name='home'),
-    path('pagina_convidativa', pagina_convidativa, name='pagina_convidativa'),
+    path('', pagina_convidativa, name='pagina_convidativa'),
 ]
