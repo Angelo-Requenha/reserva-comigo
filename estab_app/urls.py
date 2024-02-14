be0ce64be43e88a1ee7f4040259703e63635be5a
unknown <angelorequenha@gmail.com>
2024-02-14 11:46:55 -0300

    ok

--- estab_app/urls.py
+++ estab_app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import pagina_estab
 
 
 app_name = 'estab_app'
 
 urlpatterns = [
-
+    path('pagina_estab/<str:info_especifica>/', pagina_estab, name='pagina_estab'),
 ]
